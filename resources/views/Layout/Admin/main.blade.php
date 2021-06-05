e18f9b93000b4011b8e240df73b29a6bff56e1f7
DangNgocTu <t4dang1996@gmail.com>
2021-06-05 14:58:12 +0700

    favicon

--- resources/views/Layout/Admin/main.blade.php
+++ resources/views/Layout/Admin/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/png" href="{{asset('/img/images_app/favico.png')}}">
+    <link rel="icon" type="image/png" href="{{asset('/assets/images/favicon.ico')}}">
     <title>@yield('page_title')</title>
    @include('Layout.Admin.main_css')
   </head>
